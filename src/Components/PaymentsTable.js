88af5f3ebb8ab601894dc6578075b4cc8c1f5a05
florenciasilva <hensgrej@gmail.com>
2020-02-07 18:35:03 -0600

    Display JSON data in table

---
+++ src/Components/PaymentsTable.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PaymentsTable = (props) => {
+
+    const dataMap = props.data.data.payments.map((record, i) => {
+        const amount = record.amount.toString();
+        const lastTwoDigits = amount.substr(-2);
+        const firstDigits = amount.slice(0, -2);
+        const formattedAmount = firstDigits + '.' + lastTwoDigits;
+        return (
+            <tr>
+                <td>date</td>
+                <td>
+                    <p>{record.status}</p>
+                    <p style={{display: record.failure !== null ? 'block' : 'none'}}>{record.failure}</p>
+                </td>
+                <td>
+                    <p>{record.type}</p>
+                    <p>{record.brand}</p>
+                </td>
+                <td>
+                    <p>{record.customer.name}</p>
+                    <p>{record.customer.email}</p>
+                </td>
+                <td>
+                <p>{formattedAmount}</p>
+                </td>
+            </tr>
+        )
+    })
+
+    return (
+        <Table>
+            <thead>
+                <th>Date</th>                
+                <th>Payment Status</th>                
+                <th>Payment Type</th>                
+                <th>Customer</th>                
+                <th>Amount</th>            
+            </thead>
+            <tbody>
+                {dataMap}
+            </tbody>
+        </Table>
+    );
+};
+
+
+const Table = styled.table`
+    max-height: 30vh;
+    margin: 0;
+    padding: 0;
+`
+
+export default PaymentsTable;
--- src/Views/Payments.js
+++ src/Views/Payments.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import PaymentsTable from '../Components/PaymentsTable';
+import records from '../records.json';
 
-const Payments = () => {
+const Payments = () => {    
     return (
-        <p>Payments view</p>
+        <Main>
+            <Title>Payments</Title>
+            <Section>
+                <div>
+                    <PaymentsTable data={records} />
+                </div>
+            </Section>
+        </Main>
     );
 };
 
+const Main = styled.main`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    flex-direction: column;
+    margin-left: 1em;
+`
+
+const Title = styled.h1`
+    align-self: flex-start;
+    padding: 1em;
+`
+
+const Section = styled.section`
+    background-color: white;
+    width: 95%;
+    max-height: 60vh;
+    overflow-y: scroll;
+`
 
 export default Payments;
